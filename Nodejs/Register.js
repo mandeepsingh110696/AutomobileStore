0d3d0155263cfbc3667e73f6a7824f8bcf060eaf
unknown <manhack7897@gmail.com>
2021-05-15 16:28:15 -0400

    login registration api's done by harsh

--- Nodejs/Register.js
+++ Nodejs/Register.js
@@ -1,3 +1,4 @@
+// all node api's
 // import required modules
 const express = require("express");
 const cors = require("cors");
@@ -72,7 +73,112 @@ app.get("/login/", (req, res) => {
   });
 });
 
+app.post("/adminloginn", (req, res, result) => {
+  console.log(`login admin ... `);
 
+  console.log("body", req.body);
+  var query = `SELECT * FROM admin WHERE email ='${req.body.email}'and password='${req.body.password}'`;
+  console.log(query);
+  connection.query(query, (err, result, rows) => {
+    if (result && result.length > 0) {
+      res.json({ message: "admin login successfull", errorcode: "200" });
+    } else {
+      res.json({ message: "Invalid username or password", errorcode: "403" });
+    }
+  });
+});
+
+// Insert a car
+app.post("/AddCar", (req, res) => {
+  console.log(`Inserting car ... `);
+
+  console.log("body", req.body);
+
+  connection.query(
+    `INSERT INTO inventory (brand,model,price,color,year,type,Image) VALUES  ('${req.body.brand}','${req.body.model}','${req.body.price}','${req.body.color}','${req.body.year}','${req.body.type}','${req.body.img}')`,
+    (err, rows) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+});
+
+// Update a car
+app.put("/editCar/:id", (req, res) => {
+  console.log("params:", req.params);
+  const id = req.params.id;
+  console.log("body:", req.body);
+
+  connection.query(
+    `UPDATE inventory SET brand = '${req.body.brand}', model = '${req.body.model}',
+      price = '${req.body.price}',color = '${req.body.color}',year = '${req.body.year}',type = '${req.body.type}',Image = '${req.body.img}' ` +
+      `WHERE InvtId = ${id}`,
+    (err, rows) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+});
+
+// Delete a car
+app.delete("/deleteCar/:id", (req, res, result) => {
+  console.log(`Delete car id [${req.params.id}]... `);
+
+  connection.query(
+    `DELETE FROM inventory WHERE InvtId = ${req.params.id}`,
+    (err, result, fields) => {
+      if (err) throw err;
+      console.log(result.affectedRows);
+      if (result.affectedRows) {
+        res.json({ message: "Car Deleted Successfully", errorcode: "200" });
+
+        console.log("Car Deleted Successfully");
+      } else {
+        res.json({ message: "Invalid Inventory Id", errorcode: "403" });
+      }
+    }
+  );
+});
+
+// View Car By Id
+app.get("/viewCar/:id", (req, res) => {
+  console.log("params:", req.params);
+
+  const id = req.params.id;
+  console.log(`Fetching Car with id ${id}...`);
+
+  connection.query(
+    `SELECT * FROM inventory WHERE InvtId = ${id}`,
+    (err, rows) => {
+      if (err) throw err;
+      if (rows.length > 0) {
+        res.json(rows);
+        console.log("Car retreived Successfully");
+      } else {
+        res.json({ message: "Invalid Inventory Id", errorcode: "403" });
+      }
+    }
+  );
+});
+
+
+// Get All cars with filter
+app.get("/viewAllCars/:filter", (req, res) => {
+  var sql = "";
+  console.log("Fetching all cars...");
+  var filter = req.params.filter;
+  console.log(req.params);
+  if (filter == "all") {
+    sql = "SELECT * FROM inventory ";
+  } else {
+    sql = "SELECT * FROM inventory where type='" + filter + "'";
+  }
+  connection.query(sql, (err, rows) => {
+    if (err) throw err;
+    console.log("rows", rows);
+    res.json(rows);
+  });
+});
 // Listen
 app.listen(port, () => {
   console.log(`Student app listening at http://localhost:${port}`);
